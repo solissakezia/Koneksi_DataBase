f5506ccfd4fa8f85b5e25384492605fc98f554f1
solissakezia <solissakezia@gmail.com>
2026-03-10 19:35:57 +0800

    Add files via upload

---
+++ FrmFoto.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace DBKoneksi
+{
+    partial class FrmFoto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.CBLevel = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LblFoto = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Pengguna";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nama Pengguna";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Username";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Level";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(181, 99);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(181, 26);
+            this.txtUsername.TabIndex = 5;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(181, 189);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(181, 26);
+            this.txtNama.TabIndex = 7;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(181, 142);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(181, 26);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(181, 47);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(181, 26);
+            this.txtID.TabIndex = 9;
+            // 
+            // CBLevel
+            // 
+            this.CBLevel.FormattingEnabled = true;
+            this.CBLevel.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.CBLevel.Location = new System.Drawing.Point(181, 237);
+            this.CBLevel.Name = "CBLevel";
+            this.CBLevel.Size = new System.Drawing.Size(91, 28);
+            this.CBLevel.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(500, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(246, 251);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // LblFoto
+            // 
+            this.LblFoto.AutoSize = true;
+            this.LblFoto.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblFoto.Location = new System.Drawing.Point(578, 142);
+            this.LblFoto.Name = "LblFoto";
+            this.LblFoto.Size = new System.Drawing.Size(84, 20);
+            this.LblFoto.TabIndex = 12;
+            this.LblFoto.Text = "Pilih Foto";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(63, 324);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(156, 47);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(550, 324);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(156, 47);
+            this.btnSearch.TabIndex = 14;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(307, 324);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(156, 47);
+            this.btnUpdate.TabIndex = 15;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(63, 421);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(156, 47);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(307, 421);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(156, 47);
+            this.btnDelete.TabIndex = 17;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(550, 421);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(156, 47);
+            this.btnLogout.TabIndex = 18;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // FrmFoto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 535);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.LblFoto);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CBLevel);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmFoto";
+            this.Text = "FrmFoto";
+            this.Load += new System.EventHandler(this.FrmFoto_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.ComboBox CBLevel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label LblFoto;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
---
+++ FrmFoto.cs
@@ -0,0 +1,329 @@
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBKoneksi
+{
+    public partial class FrmFoto : Form
+    {
+        private MySqlConnection koneksi;
+        private MySqlDataAdapter adapter;
+        private MySqlCommand perintah;
+        private DataSet ds = new DataSet();
+        private string alamat, query;
+        
+        public FrmFoto()
+        {
+            alamat = "server=localhost; database=db_mahasiswa; username=root; password=;";
+            koneksi = new MySqlConnection(alamat);
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUsername.Text != "")
+                {
+                    query = string.Format("select * from tbl_pengguna where username = '{0}'", txtUsername.Text);
+                    ds.Clear();
+                    koneksi.Open();
+                    perintah = new MySqlCommand(query, koneksi);
+                    adapter = new MySqlDataAdapter(perintah);
+                    perintah.ExecuteNonQuery();
+                    adapter.Fill(ds);
+                    koneksi.Close();
+                    if (ds.Tables[0].Rows.Count > 0)
+                    {
+                        foreach (DataRow kolom in ds.Tables[0].Rows)
+                        {
+                            txtID.Text = kolom["id_pengguna"].ToString();
+                            txtPassword.Text = kolom["password"].ToString();
+                            txtNama.Text = kolom["nama_pengguna"].ToString();
+                            CBLevel.Text = kolom["level"].ToString();
+                            string fileName = kolom["foto"].ToString();
+
+                            string folderPath = Path.Combine(Application.StartupPath, @"C:\Users\HP\source\repos\DBKoneksi\DBKoneksi\Foto");
+                            string filePath = Path.Combine(folderPath, fileName);
+
+                            // Cek apakah file foto ada
+                            if (File.Exists(filePath))
+                            {
+                                // Tampilkan gambar di PictureBox
+                                pictureBox1.Image = Image.FromFile(filePath);
+                                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                            }
+                            else
+                            {
+                                MessageBox.Show("File gambar tidak ditemukan.");
+                            }
+
+
+
+                        }
+                        btnSave.Enabled = false;
+                        btnUpdate.Enabled = true;
+                        btnDelete.Enabled = true;
+                        btnSearch.Enabled = false;
+                        btnClear.Enabled = true;
+                        LblFoto.Visible = false;
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data Tidak Ada !!");
+                        FrmFoto_Load(null, null);
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Data Yang Anda Pilih Tidak Ada !!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Image Files|.jpg;*.jpeg;*.png;*.bmp";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            LblFoto.Visible = false;
+        }
+
+      
+            private void FrmFoto_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Pastikan koneksi tertutup sebelum dibuka
+                if (koneksi.State == ConnectionState.Open) { koneksi.Close(); }
+                koneksi.Open();
+
+                query = "select * from tbl_pengguna";
+                perintah = new MySqlCommand(query, koneksi);
+                adapter = new MySqlDataAdapter(perintah);
+
+                ds.Clear();
+                adapter.Fill(ds); // ExecuteNonQuery() dihapus karena tidak perlu untuk SELECT
+
+                txtID.Clear();
+                txtNama.Clear();
+                txtPassword.Clear();
+                txtUsername.Clear();
+                txtID.Focus();
+                btnUpdate.Enabled = false;
+                btnDelete.Enabled = false;
+                btnClear.Enabled = false;
+                btnSave.Enabled = true;
+                btnSearch.Enabled = true;
+                pictureBox1.Image = null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Load Data: " + ex.Message);
+            }
+            finally
+            {
+                // Blok ini akan SELALU DILAKUKAN meskipun terjadi error
+                koneksi.Close();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtPassword.Text != "" && txtNama.Text != "" && txtUsername.Text != "" && txtID.Text != "")
+                {
+                    // Tentukan folder tempat menyimpan gambar
+                    string folderPath = Path.Combine(Application.StartupPath, "C:\\Users\\HP\\source\\repos\\DBKoneksi\\DBKoneksi\\Foto");
+
+                    // Pastikan folder ada, jika tidak, buat folder
+                    if (!Directory.Exists(folderPath))
+                    {
+                        Directory.CreateDirectory(folderPath);
+                    }
+
+                    // Membuat nama unik untuk file gambar agar tidak tertimpa
+                    string fileName = Guid.NewGuid().ToString() + ".jpg";
+                    string filePath = Path.Combine(folderPath, fileName);
+
+                    // Simpan gambar dari PictureBox ke folder
+                    pictureBox1.Image.Save(filePath);
+
+
+                    query = string.Format("update tbl_pengguna set password = '{0}', nama_pengguna = '{1}', level = '{2}', foto = '{3}' where id_pengguna = '{4}'", txtPassword.Text, txtNama.Text, CBLevel.Text, fileName, txtID.Text);
+
+
+                    koneksi.Open();
+                    perintah = new MySqlCommand(query, koneksi);
+                    adapter = new MySqlDataAdapter(perintah);
+                    int res = perintah.ExecuteNonQuery();
+                    koneksi.Close();
+                    if (res == 1)
+                    {
+                        MessageBox.Show("Update Data Suksess ...");
+                        FrmFoto_Load(null, null);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Gagal Update Data . . . ");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Data Tidak lengkap !!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtID.Text != "")
+                {
+                    if (MessageBox.Show("Anda Yakin Menghapus Data Ini ??", "Warning", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        query = string.Format("Delete from tbl_pengguna where id_pengguna = '{0}'", txtID.Text);
+                        ds.Clear();
+                        koneksi.Open();
+                        perintah = new MySqlCommand(query, koneksi);
+                        adapter = new MySqlDataAdapter(perintah);
+                        int res = perintah.ExecuteNonQuery();
+                        koneksi.Close();
+                        if (res == 1)
+                        {
+                            MessageBox.Show("Delete Data Suksess ...");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Gagal Delete data");
+                        }
+                    }
+                    FrmFoto_Load(null, null);
+                }
+                else
+                {
+                    MessageBox.Show("Data Yang Anda Pilih Tidak Ada !!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FrmFoto_Load(null, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Hide();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUsername.Text != "" && txtPassword.Text != "" && txtNama.Text != "" && pictureBox1.Image != null)
+                {
+                    // Perbaikan Path Folder (Lebih rapi)
+                    string folderPath = @"C:\Users\HP\source\repos\DBKoneksi\DBKoneksi\Foto";
+
+                    // Pastikan folder ada, jika tidak, buat folder
+                    if (!Directory.Exists(folderPath))
+                    {
+                        Directory.CreateDirectory(folderPath);
+                    }
+
+                    // Membuat nama unik untuk file gambar
+                    string fileName = Guid.NewGuid().ToString() + ".jpg";
+                    string filePath = Path.Combine(folderPath, fileName);
+
+                    // Simpan gambar dari PictureBox ke folder
+                    pictureBox1.Image.Save(filePath);
+
+                    // MENGGUNAKAN PARAMETER UNTUK MENCEGAH ERROR SYNTAX & FORMAT
+                    query = "insert into tbl_pengguna values (@id, @user, @pass, @nama, @level, @file)";
+
+                    if (koneksi.State == ConnectionState.Open) { koneksi.Close(); }
+                    koneksi.Open();
+
+                    perintah = new MySqlCommand(query, koneksi);
+
+                    // Memasukkan isi textbox ke parameter query
+                    perintah.Parameters.AddWithValue("@id", txtID.Text);
+                    perintah.Parameters.AddWithValue("@user", txtUsername.Text);
+                    perintah.Parameters.AddWithValue("@pass", txtPassword.Text);
+                    perintah.Parameters.AddWithValue("@nama", txtNama.Text);
+                    perintah.Parameters.AddWithValue("@level", CBLevel.Text);
+                    perintah.Parameters.AddWithValue("@file", fileName);
+
+                    // adapter = new MySqlDataAdapter(perintah); // <-- Dihapus karena INSERT tidak butuh DataAdapter
+
+                    int res = perintah.ExecuteNonQuery();
+
+                    if (res == 1)
+                    {
+                        MessageBox.Show("Insert Data Sukses ...");
+                        FrmFoto_Load(null, null); // Refresh tampilan
+                    }
+                    else
+                    {
+                        MessageBox.Show("Gagal insert Data ...");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Data Tidak lengkap !! Pastikan semua kolom terisi dan foto sudah dipilih.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Simpan: " + ex.Message);
+            }
+            finally
+            {
+                // Pastikan koneksi selalu tertutup walaupun ada error saat simpan
+                if (koneksi.State == ConnectionState.Open) { koneksi.Close(); }
+            }
+        }
+    }
+    
+}
